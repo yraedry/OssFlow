e614d9877e447d747de04d2c8f3e9a74de4932d1
adrian nuñez <adrianns93@gmail.com>
2026-05-08 12:29:59 +0200

    feat: seed YouTube URLs para ejercicios físicos (V221) Añade URLs de YouTube reales para los 30 ejercicios más comunes del catálogo. La migración V221 actualiza los registros existentes.

---
+++ src/main/resources/db/migration/V221__seed_exercises_youtube_urls.sql
@@ -0,0 +1,33 @@
+-- V221__seed_exercises_youtube_urls.sql
+-- YouTube URLs para ejercicios físicos del seed (owner_id=1).
+
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=IODxDxX7oi4' WHERE name = 'Push-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=J0DnG1_S92I' WHERE name = 'Diamond Push-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=Wc2_xrQNnQQ' WHERE name = 'Pike Push-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=aclHkVaku9U' WHERE name = 'Bodyweight Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=U4s4mEQ5VOU' WHERE name = 'Jump Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=2C-uNgKwPLE' WHERE name = 'Bulgarian Split Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=vq5-vdgJc0I' WHERE name = 'Pistol Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=qnMKMFrX1nU' WHERE name = 'Plank' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=swFJDMSv7cg' WHERE name = 'Hollow Body Hold' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=LDBjPCHSRHo' WHERE name = 'L-Sit' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=NG9qbvAN3gQ' WHERE name = 'V-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=1919eTCoESo' WHERE name = 'Mountain Climber' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=XxWcirHIwVo' WHERE name = 'Burpee' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=UtN0OoJVTqA' WHERE name = 'Pull-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=pYcpY20QaE8' WHERE name = 'Chin-Up' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=rloXYB8M3vU' WHERE name = 'Dips' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo' WHERE name = 'Hanging Knee Raise' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=ZB1SwBRVLCc' WHERE name = 'Kettlebell Swing' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=nhoikoUEI8U' WHERE name = 'Goblet Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=7F4CsX9eWAw' WHERE name = 'Romanian Deadlift' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=wUoh3VonDCo' WHERE name = 'Barbell Squat' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=ytGaGIn3SjE' WHERE name = 'Barbell Deadlift' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=gRVjAtPip0Y' WHERE name = 'Bench Press' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=2dxMzrHmOKU' WHERE name = 'Barbell Row' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=oxFISOiaMOE' WHERE name = 'Overhead Press' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=r3pom1Ww_4E' WHERE name = 'Hip Thrust' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=cDJGHOpfnpY' WHERE name = 'Farmer Walk' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=ep2aRPSfRFY' WHERE name = 'Battle Ropes' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=u3zgHI8QnqE' WHERE name = 'Box Jump' AND owner_id = 1;
+UPDATE exercise SET youtube_url = 'https://www.youtube.com/watch?v=wDRBDEQ4VYo' WHERE name = 'Assault Bike' AND owner_id = 1;
